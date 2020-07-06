ad24fc1e7e985a76e03f171a34b879b9d8636228
Timothy Yap <tim@timaaronyap.com>
2020-07-06 21:42:33 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 def display_rainbow(colors)
-  puts ("R: #{color[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]})""
+  puts ("R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]})""
 end
 
-display_rainbow
+display_rainbow(colors)
