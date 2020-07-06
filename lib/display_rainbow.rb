345b71855f90f03c38ea64f84e402ccf63e56052
Timothy Yap <tim@timaaronyap.com>
2020-07-06 22:21:33 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
-def display_rainbow('red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet')
+def display_rainbow(colors)
 
 colors= ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
